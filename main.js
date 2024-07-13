c46e6896c3e280d12fe3ca1a281ca1ee68aed130
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-07-13 14:24:19 +0100

    Main.js Modified

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const mortageTerm = document.querySelector('[data-mortageTerm]')
 const mortageInterest = document.querySelector('[data-mortageInterest]')
 const mortageTypeInterest = document.querySelector('[data-mortageTypeInterest]')
 const mortageTypeRepayment = document.querySelector('[data-mortageTypeRepayment]')
+
 const outputMonthlyRepayment = document.querySelector('[data-monthlyRepayment]')
 const outputTotalRepayment = document.querySelector('[data-totalPayment]')
 const empty = document.querySelector('[data-emptyResult]');
 const complete = document.querySelector('[data-compleatedResult]');
+
 formTag.addEventListener('submit', (event)=>{
     event.preventDefault()
     const yearlyInterest = ((parseInt(mortageAmount.value) * parseInt(mortageInterest.value))/100).toFixed(2)
@@ -28,7 +30,11 @@ formTag.addEventListener('submit', (event)=>{
     complete.classList.remove('d-none');
     return;
 })
+
 resetForm.addEventListener('click', (event)=>{
     event.preventDefault();
     formTag.reset();
+    empty.classList.remove('d-none');
+    complete.classList.add('d-none');
+    return;
 })
