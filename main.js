85e158649662b009961d8407feca9d3769fd91f5
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-07-13 14:23:12 +0100

    Update Added

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ const mortageTypeInterest = document.querySelector('[data-mortageTypeInterest]')
 const mortageTypeRepayment = document.querySelector('[data-mortageTypeRepayment]')
 const outputMonthlyRepayment = document.querySelector('[data-monthlyRepayment]')
 const outputTotalRepayment = document.querySelector('[data-totalPayment]')
-
+const empty = document.querySelector('[data-emptyResult]');
+const complete = document.querySelector('[data-compleatedResult]');
 formTag.addEventListener('submit', (event)=>{
     event.preventDefault()
     const yearlyInterest = ((parseInt(mortageAmount.value) * parseInt(mortageInterest.value))/100).toFixed(2)
@@ -19,10 +20,13 @@ formTag.addEventListener('submit', (event)=>{
         outputMonthlyRepayment.innerText = `\u20AC ${monthlyRepayment}`
         outputTotalRepayment.innerText = `\u20AC ${totalYearlyRepayment}`
     }
-    if(mortageTypeInterest.checked){
+    else if(mortageTypeInterest.checked){
         outputMonthlyRepayment.innerText = `\u20AC ${monthlyInterst}`
         outputTotalRepayment.innerText = `\u20AC ${totalYearlyInterest}`
-    }return
+    }
+    empty.classList.add('d-none');
+    complete.classList.remove('d-none');
+    return;
 })
 resetForm.addEventListener('click', (event)=>{
     event.preventDefault();
