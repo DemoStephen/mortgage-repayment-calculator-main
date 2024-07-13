7e4bb4dd060deb4aac6fe6df4313804180aaba73
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-07-13 10:08:12 +0100

    Done with Responsiveness, Working on functionality

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-const numberInput = document.querySelector('[data-mortageAmount]');
-const submit = document.querySelector('[data-form]');
-let number = 198200780;
-let newStr = number.toString();
-let popped;
-for(let i = 0; i < newStr.length; i +=3){
-    popped = newStr.pop(newStr)
-}
-submit.addEventListener('submit', (event)=>{
-    event.preventDefault()
-    console.log(`${numberInput.value}`);
-})
+const resetForm = document.querySelector('[data-reset]');
+const formTag = document.querySelector('[data-form]');
+const mortageAmount = document.querySelector('[data-mortageAmount]');
+const mortageTerm = document.querySelector('[data-mortageTerm]');
+const mortageInterest = document.querySelector('[data-mortageInterest]');
+const mortageTypeInterest = document.querySelector('[data-mortageTypeRepayment]');
+const mortageTypeRepayment = document.querySelector('[data-mortageTypeInterest]');
+
+const outputMonthlyRepayment = document.querySelector('[data-monthlyRepayment]');
+const outputTotalRepayment = document.querySelector('[data-totalPayment]');
+
+formTag.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    console.log(`${mortageAmount.value} ${mortageTerm.value} ${mortageInterest.value}`)
+});
