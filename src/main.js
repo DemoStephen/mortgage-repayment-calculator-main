cf5bfd38533300d1150386de845456f50329bbe2
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-12-09 13:13:29 +0100

    Working on seperation of concerns

--- src/main.js
+++ src/main.js
@@ -1,37 +1,38 @@
 const form = document.querySelector("[data-form]");
 const err = form.querySelectorAll("[data-error]");
+const emptyState = document.querySelector("[data-empty]");
+const successState = document.querySelector("[data-completed]");
+
+function handleOutput(data) {
+  const outputType = document.querySelector(["data-type-output"]);
+  const repaymentMonthly = document.querySelector(["data-monthly-repayment"]);
+  const repaymentTotally = document.querySelector(["data-total-payment"]);
+  console.log(data);
+  if (data.amount && data.term && data.interest && data.type) {
+    emptyState.classList.add("hidden");
+    successState.classList.remove("hidden");
+
+    outputType.innerText = "";
+    repaymentMonthly.innerText = "";
+    repaymentTotally.innerText = "";
+  } else {
+    emptyState.classList.remove("hidden");
+    successState.classList.add("hidden");
+  }
+}
+
+function handleError(data) {
+  const err = document.querySelectorAll("[data-error]");
+  console.log(err);
+}
 
 function handleSubmit(event) {
   event.preventDefault();
-
   const fd = new FormData(event.target);
   const type = fd.get("type");
   const data = Object.fromEntries(fd.entries());
   data.type = type;
-  console.log(data);
-  if (data.amount === "") {
-    err[0].classList.remove("hidden");
-    console.log("empty");
-  } else {
-    err[0].classList.add("hidden");
-  }
-  if (data.term === "") {
-    err[1].classList.remove("hidden");
-    console.log("empty");
-  } else {
-    err[1].classList.add("hidden");
-  }
-  if (data.interest === "") {
-    err[2].classList.remove("hidden");
-    console.log("empty");
-  } else {
-    err[2].classList.add("hidden");
-  }
-  if (data.type === null) {
-    err[2].classList.remove("hidden");
-    console.log("empty");
-  } else {
-    err[2].classList.add("hidden");
-  }
+  handleError(data);
+  handleOutput(data);
 }
 form.addEventListener("submit", handleSubmit);
